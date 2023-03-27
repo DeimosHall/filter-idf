a6f023a23f9fbe78dec63ecb64c07be46197c3fb
Deimos <deimoshall@gmail.com>
2023-03-26 21:20:17 -0600

    refactor: add ticks to time functions

--- src/main.c
+++ src/main.c
@@ -8,7 +8,10 @@
 #define ADC 33
 #define DAC 25
 
-void testOneSecond();
+void test_one_second();
+uint32_t ticks_to_ms(uint32_t ticks);
+uint32_t ticks_to_us(uint32_t ticks);
+uint32_t ticks_to_ns(uint32_t ticks);
 
 void app_main() {
     esp_rom_gpio_pad_select_gpio(ADC);
@@ -17,34 +20,41 @@ void app_main() {
     gpio_set_direction(DAC, GPIO_MODE_OUTPUT);
 
     int core = esp_cpu_get_core_id();
-    int cycle_count = esp_cpu_get_cycle_count();
     int cpu_freq = esp_rom_get_cpu_ticks_per_us();
     printf("Core: %d\n", core);
-    printf("Cycle count: %d\n", cycle_count);
     printf("CPU freq: %d MHz\n", cpu_freq);
 
-    testOneSecond();
+    test_one_second();
 
     for (int i = 0; i < 20; i++) {
         uint32_t start = xthal_get_ccount();
-        // uint32_t start = esp_cpu_get_cycle_count();
         int adc = gpio_get_level(ADC);
 
         gpio_set_level(DAC, adc);
         uint32_t end = xthal_get_ccount();
-        // uint32_t end = esp_cpu_get_cycle_count();
-        
-        uint32_t elapsed_time = (end - start) * 1000 / cpu_freq;
+
+        uint32_t elapsed_time = ticks_to_ns(end - start);
         printf("Elapsed time: %ld ns\n", elapsed_time);
     }
 }
 
-void testOneSecond() {
+void test_one_second() {
     uint32_t start = xthal_get_ccount();
     vTaskDelay(1000 / portTICK_PERIOD_MS); // 1 second delay
     uint32_t end = xthal_get_ccount();
 
-    uint32_t cpu_freq = esp_rom_get_cpu_ticks_per_us();
-    printf("Test 1 second: %ld us\n", (end - start) / cpu_freq);
-    printf("Test 1 second: %ld ms\n", (end - start) / cpu_freq * 1000);
+    printf("Test 1 second: %ld us\n", ticks_to_us(end - start));
+    printf("Test 1 second: %ld ms\n", ticks_to_ms(end - start));
+}
+
+uint32_t ticks_to_ms(uint32_t ticks) {
+    return ticks / (esp_rom_get_cpu_ticks_per_us() * 1000);
+}
+
+uint32_t ticks_to_us(uint32_t ticks) {
+    return ticks / esp_rom_get_cpu_ticks_per_us();
+}
+
+uint32_t ticks_to_ns(uint32_t ticks) {
+    return ticks * 1000 / esp_rom_get_cpu_ticks_per_us();
 }
